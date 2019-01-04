32a998b4be10291896c95bf6cd727488f2f89ad3
Peter Ramos <peterdramos02@gmail.com>
2019-01-04 12:28:20 -0500

    add POST route to tags routes and test with postman

--- db/noteful-app.sql
+++ db/noteful-app.sql
@@ -1,111 +1,111 @@
 -- psql -U dev -d noteful-app -f ./db/noteful-app.sql
 
--- DROP TABLE IF EXISTS notes_tags;
--- DROP TABLE IF EXISTS notes;
--- DROP TABLE IF EXISTS folders;
--- DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS notes_tags;
+DROP TABLE IF EXISTS notes;
+DROP TABLE IF EXISTS folders;
+DROP TABLE IF EXISTS tags;
 
--- CREATE TABLE folders (
---   id serial PRIMARY KEY,
---   name text NOT NULL
--- );
+CREATE TABLE folders (
+  id serial PRIMARY KEY,
+  name text NOT NULL
+);
 
--- CREATE TABLE notes (
---   id serial PRIMARY KEY,
---   title text NOT NULL,
---   content text,
---   created timestamp DEFAULT now(),
---   folder_id int REFERENCES folders(id) ON DELETE SET NULL
--- );
+CREATE TABLE notes (
+  id serial PRIMARY KEY,
+  title text NOT NULL,
+  content text,
+  created timestamp DEFAULT now(),
+  folder_id int REFERENCES folders(id) ON DELETE SET NULL
+);
 
--- CREATE TABLE tags (
---   id serial PRIMARY KEY,
---   name text UNIQUE NOT NULL
--- );
+CREATE TABLE tags (
+  id serial PRIMARY KEY,
+  name text UNIQUE NOT NULL
+);
 
--- CREATE TABLE notes_tags (
---   note_id INTEGER NOT NULL REFERENCES notes ON DELETE CASCADE,
---   tag_id INTEGER NOT NULL REFERENCES tags ON DELETE CASCADE
--- );
+CREATE TABLE notes_tags (
+  note_id INTEGER NOT NULL REFERENCES notes ON DELETE CASCADE,
+  tag_id INTEGER NOT NULL REFERENCES tags ON DELETE CASCADE
+);
 
--- ALTER SEQUENCE folders_id_seq RESTART WITH 100;
--- ALTER SEQUENCE notes_id_seq RESTART WITH 1000;
+ALTER SEQUENCE folders_id_seq RESTART WITH 100;
+ALTER SEQUENCE notes_id_seq RESTART WITH 1000;
 
--- INSERT INTO folders (name) VALUES
---   ('Archive'),
---   ('Drafts'),
---   ('Personal'),
---   ('Work');
+INSERT INTO folders (name) VALUES
+  ('Archive'),
+  ('Drafts'),
+  ('Personal'),
+  ('Work');
 
--- INSERT INTO notes (title, content, folder_id) VALUES 
---   (
---     '5 life lessons learned from cats',
---     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
---     100
---   ),
---   (
---     'What the government doesn''t want you to know about cats',
---     'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.',
---     103
---   ),
---   (
---     'The most boring article about cats you''ll ever read',
---     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
---     101
---   ),
---   (
---     '7 things lady gaga has in common with cats',
---     'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.',
---     102
---   ),
---   (
---     'The most incredible article about cats you''ll ever read',
---     'Lorem ipsum dolor sit amet, boring consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
---     102
---   ),
---   (
---     '10 ways cats can help you live to 100',
---     'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.',
---     103
---   ),
---   (
---     '9 reasons you can blame the recession on cats',
---     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
---     100
---   ),
---   (
---     '10 ways marketers are making you addicted to cats',
---     'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.',
---     103
---   ),
---   (
---     '11 ways investing in cats can make you a millionaire',
---     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
---     101
---   ),
---   (
---     'Why you should forget everything you learned about cats',
---     'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.',
---     null
---   );
+INSERT INTO notes (title, content, folder_id) VALUES 
+  (
+    '5 life lessons learned from cats',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    100
+  ),
+  (
+    'What the government doesn''t want you to know about cats',
+    'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.',
+    103
+  ),
+  (
+    'The most boring article about cats you''ll ever read',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    101
+  ),
+  (
+    '7 things lady gaga has in common with cats',
+    'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.',
+    102
+  ),
+  (
+    'The most incredible article about cats you''ll ever read',
+    'Lorem ipsum dolor sit amet, boring consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    102
+  ),
+  (
+    '10 ways cats can help you live to 100',
+    'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.',
+    103
+  ),
+  (
+    '9 reasons you can blame the recession on cats',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    100
+  ),
+  (
+    '10 ways marketers are making you addicted to cats',
+    'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.',
+    103
+  ),
+  (
+    '11 ways investing in cats can make you a millionaire',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    101
+  ),
+  (
+    'Why you should forget everything you learned about cats',
+    'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.',
+    null
+  );
 
---   INSERT INTO tags (name) VALUES
---   ('Grocery'),
---   ('Coffee'),
---   ('Pet');
+  INSERT INTO tags (name) VALUES
+  ('Grocery'),
+  ('Coffee'),
+  ('Pet');
 
---   INSERT INTO notes_tags (note_id, tag_id) VALUES
---   (1001, 1),
---   (1002, 1),
---   (1003, 2),
---   (1005, 1),
---   (1005, 2),
---   (1006, 1),
---   (1007, 3),
---   (1007, 1),
---   (1009, 3);
+  INSERT INTO notes_tags (note_id, tag_id) VALUES
+  (1001, 1),
+  (1002, 1),
+  (1003, 2),
+  (1005, 1),
+  (1005, 2),
+  (1006, 1),
+  (1007, 3),
+  (1007, 1),
+  (1009, 3);
 
-SELECT title, tags.name, folders.name FROM notes
-LEFT JOIN folders ON notes.folder_id = folders.id
-LEFT JOIN notes_tags ON notes.id = notes_tags.note_id
-LEFT JOIN tags ON notes_tags.tag_id = tags.id;
+-- SELECT title, tags.name, folders.name FROM notes
+-- LEFT JOIN folders ON notes.folder_id = folders.id
+-- LEFT JOIN notes_tags ON notes.id = notes_tags.note_id
+-- LEFT JOIN tags ON notes_tags.tag_id = tags.id;
--- routes/tags.js
+++ routes/tags.js
@@ -15,6 +15,7 @@ router.get('/tags', (req, res, next) => {
     .catch(err => next(err));
 });
 
+// GET single tag by id
 router.get('/tags/:id', (req, res, next) => {
   const { id } = req.params;
 
@@ -27,5 +28,28 @@ router.get('/tags/:id', (req, res, next) => {
     .catch(err => next(err));
 });
 
+// Create/POST(insert) a tag
+router.post('/tags', (req, res, next) => {
+  const { name } = req.body;
+
+  /****** Never trust users. Validate input *******/
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const newItem = { name };
+
+  knex
+    .insert(newItem)
+    .into('tags')
+    .returning(['id', 'name'])
+    .then(results => {
+      const result = results[0];
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(err => next(err));
+});
 
 module.exports = router;
